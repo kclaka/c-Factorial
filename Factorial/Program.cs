d294226d3392af4ef29bf4f4750bc8f0f18488d6
kclaka <kennyigbechi@gmail.com>
2020-03-31 13:09:31 -0700

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            if (number < 1) return 0;
+
+            else if (number == 1) return 1;
+
+            else return number * Factorial(number - 1);
+        }
+
+        static void RunFactorial()
+        {
+            bool isNumber;
+            do
+            {
+                Console.WriteLine("Enter a Number: ");
+                isNumber = Int32.TryParse(Console.ReadLine(), out int intNumber);
+                if (isNumber) Console.WriteLine(Factorial(intNumber));
+                else Console.WriteLine("Enter a Valid Number");
+
+            } while (isNumber);
+        }
+        static void Main(string[] args)
+        {
+            RunFactorial();
+        }
+    }
+}
